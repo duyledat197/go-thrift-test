dede9e14cf7027bc9ad3dbf2eac580c956a8d7e1
thuocsi_datle <dat.le@thuocsi.vn>
2020-12-29 15:40:21 +0700

    fix develop

--- develop/gen-script.sh
+++ develop/gen-script.sh
@@ -7,7 +7,7 @@ rm -rf $DIR/thr
 mkdir thr
 
 for pkg in $(ls -c $THRIFT_DIR); do
-    thrift -out $DIR/thr --gen go:package_prefix=github.com/duyledat197/test-thrift/thr/ $THRIFT_DIR/$pkg
+    thrift -out $DIR/thr --gen go:package_prefix=$WORK_DIR/thr/ $THRIFT_DIR/$pkg
 done
 
 for pkg in $(ls -c $DIR/thr); do
